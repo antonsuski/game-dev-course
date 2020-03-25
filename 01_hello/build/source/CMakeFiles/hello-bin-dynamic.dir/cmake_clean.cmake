e7281fe2da7f9867ce68a02090cd97ccec57da2a
Anton <antonsyski@gmail.com>
2020-03-25 12:49:44 +0300

    save changes

---
+++ 01_hello/build/source/CMakeFiles/hello-bin-dynamic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello-bin-dynamic.dir/main.cxx.o"
+  "hello-bin-dynamic"
+  "hello-bin-dynamic.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello-bin-dynamic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build-01_hello-Desktop-Debug/libs/CMakeFiles/hello-lib-dynamic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello-lib-dynamic.dir/source/hello.cxx.o"
+  "libhello-lib-dynamic.pdb"
+  "libhello-lib-dynamic.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello-lib-dynamic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
